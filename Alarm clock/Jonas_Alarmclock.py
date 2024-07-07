b83e23561f384608c8eef2403a1d93d2146c1dfc
Jonas Nielsen <jonasn0405@gmail.com>
2024-07-07 21:11:59 +0200

    Test

---
+++ Alarm clock/Jonas_Alarmclock.py
@@ -0,0 +1,11 @@
+from tkinter import *
+import datetime
+import time
+import winsound
+
+def alarm(set_alarm_timer):
+    while True:
+        time.sleep(1)
+        current_time = datetime.datetime.now()
+        now = current_time.strftime("%H:%M:%S")
+        date = current_time.strftime
